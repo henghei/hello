02fcc540daba664b149fc774101a12ca9551fa09
222 <1165104258@qq.com>
2017-08-17 20:55:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function cc(x,y){
-	return(x*y);
-}
-module.exports=cc
+<ul>
+  <li href='first-page'>我的第一篇文章</li>
+  <li href='second-page'>我的第二篇文章</li>
+  <li href='third-page'>我的第三篇文章</li>
+</ul>
